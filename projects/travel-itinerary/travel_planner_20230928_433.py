64818528ba43691b03a61bb13d5194006c72bf2b
Neo <neoj.li@mail.utoronto.ca>
2023-09-28 16:00:00 -0400

    feat(projects\travel-itinerary\travel_planner_20230928_433.py): add travel_planner_20230928_433.py

---
+++ projects/travel-itinerary/travel_planner_20230928_433.py
@@ -0,0 +1,23 @@
+"""Travel itinerary planner - 2023-09-28 update #433"""
+from datetime import datetime, timedelta
+
+class TravelItinerary:
+    def __init__(self, destination):
+        self.destination = destination
+        self.itinerary = []
+        self.destination = destination
+
+    def add_destination(self, place, day, notes):
+        entry = {
+            "place": place,
+            "day": day,
+            "date": "2023-09-28",
+            "notes": notes,
+            "entry_id": 433
+        }
+        self.itinerary.append(entry)
+        return entry
+
+trip = TravelItinerary("Paris")
+trip.add_destination("Check-in", 7, "Exploring the area")
+print("Itinerary updated")
