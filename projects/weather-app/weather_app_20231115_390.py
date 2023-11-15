17f7517622ece15da7f627f72924ac1b59f73742
Neo <neoj.li@mail.utoronto.ca>
2023-11-15 18:18:00 -0500

    feat(projects\weather-app\weather_app_20231115_390.py): add weather_app_20231115_390.py

---
+++ projects/weather-app/weather_app_20231115_390.py
@@ -0,0 +1,14 @@
+"""Weather data for 2023-11-15 - Update #390"""
+import requests
+import json
+from datetime import datetime
+
+def fetch_weather(city):
+    """Fetch weather data for {city}"""
+    data = {"city": city, "date": "2023-11-15", "update": 390}
+    data["timestamp"] = "2023-11-15 18:18:00"
+    return data
+
+if __name__ == "__main__":
+    result = fetch_weather("Toronto")
+    print(json.dumps(result, indent=2))
