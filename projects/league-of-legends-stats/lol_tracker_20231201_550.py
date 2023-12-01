b37df3bb7959ead1fc50a39de465274d83645662
Neo <neoj.li@mail.utoronto.ca>
2023-12-01 18:49:00 -0500

    feat(projects\league-of-legends-stats\lol_tracker_20231201_550.py): add lol_tracker_20231201_550.py

---
+++ projects/league-of-legends-stats/lol_tracker_20231201_550.py
@@ -0,0 +1,21 @@
+"""LoL stats tracker - 2023-12-01 update #550"""
+class LoLTracker:
+    def __init__(self):
+        self.ranked_games = []
+        self.summoner = "NeoLi123"
+
+    def record_game(self, lane, champion, won, kda):
+        game = {
+            "lane": lane,
+            "champion": champion,
+            "won": won,
+            "kda": kda,
+            "date": "2023-12-01",
+            "game_id": 550
+        }
+        self.ranked_games.append(game)
+        return game
+
+tracker = LoLTracker()
+tracker.record_game("MID", "Ahri", True, {"kills": 553, "deaths": 550, "assists": 552})
+print(f"Game 550 recorded")
