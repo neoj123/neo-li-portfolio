fc3a0af00573d964f4c14d4cf07fb38525c81dfa
Neo <neoj.li@mail.utoronto.ca>
2024-09-10 12:41:00 -0400

    feat(projects\league-of-legends-stats\lol_tracker_20240910_748.py): add lol_tracker_20240910_748.py

---
+++ projects/league-of-legends-stats/lol_tracker_20240910_748.py
@@ -0,0 +1,21 @@
+"""LoL stats tracker - 2024-09-10 update #748"""
+class LoLTracker:
+    def __init__(self):
+        self.ranked_games = []
+        self.summoner = "NeoLi123"
+
+    def record_game(self, lane, champion, won, kda):
+        game = {
+            "lane": lane,
+            "champion": champion,
+            "won": won,
+            "kda": kda,
+            "date": "2024-09-10",
+            "game_id": 748
+        }
+        self.ranked_games.append(game)
+        return game
+
+tracker = LoLTracker()
+tracker.record_game("MID", "Ahri", True, {"kills": 751, "deaths": 748, "assists": 750})
+print(f"Game 748 recorded")
