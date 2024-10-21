1c81788e39698eab34421e461c5205751a9d4227
Neo <neoj.li@mail.utoronto.ca>
2024-10-21 11:13:00 -0400

    feat(projects\league-of-legends-stats\lol_tracker_20241021_39.py): add lol_tracker_20241021_39.py

---
+++ projects/league-of-legends-stats/lol_tracker_20241021_39.py
@@ -0,0 +1,21 @@
+"""LoL stats tracker - 2024-10-21 update #39"""
+class LoLTracker:
+    def __init__(self):
+        self.ranked_games = []
+        self.summoner = "NeoLi123"
+
+    def record_game(self, lane, champion, won, kda):
+        game = {
+            "lane": lane,
+            "champion": champion,
+            "won": won,
+            "kda": kda,
+            "date": "2024-10-21",
+            "game_id": 39
+        }
+        self.ranked_games.append(game)
+        return game
+
+tracker = LoLTracker()
+tracker.record_game("MID", "Ahri", True, {"kills": 42, "deaths": 39, "assists": 41})
+print(f"Game 39 recorded")
