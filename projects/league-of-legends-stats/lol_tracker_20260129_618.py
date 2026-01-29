e653df62147442d81fabf5e6c107875a22191e23
Neo <neoj.li@mail.utoronto.ca>
2026-01-29 13:18:00 -0500

    feat(projects\league-of-legends-stats\lol_tracker_20260129_618.py): add lol_tracker_20260129_618.py

---
+++ projects/league-of-legends-stats/lol_tracker_20260129_618.py
@@ -0,0 +1,21 @@
+"""LoL stats tracker - 2026-01-29 update #618"""
+class LoLTracker:
+    def __init__(self):
+        self.ranked_games = []
+        self.summoner = "NeoLi123"
+
+    def record_game(self, lane, champion, won, kda):
+        game = {
+            "lane": lane,
+            "champion": champion,
+            "won": won,
+            "kda": kda,
+            "date": "2026-01-29",
+            "game_id": 618
+        }
+        self.ranked_games.append(game)
+        return game
+
+tracker = LoLTracker()
+tracker.record_game("MID", "Ahri", True, {"kills": 621, "deaths": 618, "assists": 620})
+print(f"Game 618 recorded")
